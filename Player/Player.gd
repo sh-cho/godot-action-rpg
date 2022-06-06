2ac72067a2729aaeb212416dba1892297c837f89
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2022-06-06 20:26:29 +0900

    Part 6 (AnimationTree)

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,21 +6,23 @@ const FRICTION := 10
 
 
 onready var anim_player := $AnimationPlayer
+onready var anim_tree := $AnimationTree
+onready var anim_state: AnimationNodeStateMachinePlayback = anim_tree.get("parameters/playback")
+
 
 var velocity := Vector2.ZERO
 
 
 func _physics_process(delta: float) -> void:
-	var input_vector := Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = input_vector.normalized()
+	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
 	if input_vector != Vector2.ZERO:
-		anim_player.play("RunRight" if input_vector.x > 0 else "RunLeft")
+		anim_tree.set("parameters/Idle/blend_position", input_vector)
+		anim_tree.set("parameters/Run/blend_position", input_vector)
+		anim_state.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION)
 	else:
-		anim_player.play("IdleRight")
+		anim_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 
 	velocity = move_and_slide(velocity)
