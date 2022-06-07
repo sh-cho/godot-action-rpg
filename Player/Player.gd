e7a36a9bd149f79ae18142a32bbedf21dae85c69
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2022-06-07 12:54:17 +0900

    Revert "Unnecessary property?"
    This reverts commit eea8469fc2623b42abe29c8aa991866af97a1862.

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,16 +10,19 @@ onready var anim_tree := $AnimationTree
 onready var anim_state: AnimationNodeStateMachinePlayback = anim_tree.get("parameters/playback")
 
 
+var velocity := Vector2.ZERO
+
+
 func _physics_process(delta: float) -> void:
-	var velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
-	if velocity != Vector2.ZERO:
-		anim_tree.set("parameters/Idle/blend_position", velocity)
-		anim_tree.set("parameters/Run/blend_position", velocity)
+	if input_vector != Vector2.ZERO:
+		anim_tree.set("parameters/Idle/blend_position", input_vector)
+		anim_tree.set("parameters/Run/blend_position", input_vector)
 		anim_state.travel("Run")
-		velocity = velocity.move_toward(velocity * MAX_SPEED, ACCELERATION)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION)
 	else:
 		anim_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 
-	move_and_slide(velocity)
+	velocity = move_and_slide(velocity)
