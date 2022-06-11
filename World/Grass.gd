8122e6bd84d367d606c3f1e1798f58a18fd9b39e
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2022-06-11 16:22:38 +0900

    Part 11 (hurtbox, hitbox)

--- World/Grass.gd
+++ World/Grass.gd
@@ -4,9 +4,12 @@ extends Node2D
 onready var parent_node := get_parent()
 
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("attack"):
-		var grass_effect := preload("res://Effects/GrassEffect.tscn").instance()
-		grass_effect.set_transform(get_transform())
-		parent_node.add_child(grass_effect)
-		queue_free()
+func create_grass_effect():
+	var grass_effect := preload("res://Effects/GrassEffect.tscn").instance()
+	grass_effect.set_transform(get_transform())
+	parent_node.add_child(grass_effect)
+
+
+func _on_Hurtbox_area_entered(area: Area2D) -> void:
+	create_grass_effect()
+	queue_free()
