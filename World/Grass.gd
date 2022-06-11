3d52faf98e236479fc50a95e61eb312d88e4acc1
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2022-06-11 15:40:16 +0900

    use snake case [skip ci]

--- World/Grass.gd
+++ World/Grass.gd
@@ -6,7 +6,7 @@ onready var parent_node := get_parent()
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("attack"):
-		var grassEffect := preload("res://Effects/GrassEffect.tscn").instance()
-		grassEffect.set_transform(get_transform())
-		parent_node.add_child(grassEffect)
+		var grass_effect := preload("res://Effects/GrassEffect.tscn").instance()
+		grass_effect.set_transform(get_transform())
+		parent_node.add_child(grass_effect)
 		queue_free()
