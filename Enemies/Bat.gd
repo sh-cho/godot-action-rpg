39d17bd4ca3ff4dea2d75cf1bc619666b80e7f76
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2022-06-12 13:27:53 +0900

    Add bat health

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 var knockback := Vector2.ZERO
 
+onready var stats = $Stats
+
 
 func _physics_process(_delta: float) -> void:
 	knockback = knockback.move_toward(Vector2.ZERO, 10)
@@ -9,4 +11,9 @@ func _physics_process(_delta: float) -> void:
 
 
 func _on_Hurtbox_area_entered(area: Area2D) -> void:
+	stats.health -= 1
 	knockback = area.knockback_vector * 200
+
+
+func _on_Stats_no_health() -> void:
+	queue_free()
