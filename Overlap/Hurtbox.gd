e760ce2d79b7fc64a5dad3e4035b625f65e7f2f6
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2022-06-12 14:51:53 +0900

    Add hit effect

---
+++ Overlap/Hurtbox.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+export(bool) var show_hit = true
+
+onready var main := get_tree().current_scene
+
+
+func _on_Hurtbox_area_entered(area: Area2D) -> void:
+	if show_hit:
+		var effect = preload("res://Effects/HitEffect.tscn").instance()
+		effect.global_position = global_position
+		main.add_child(effect)
