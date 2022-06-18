d093ca1e24eb914f328852883f1d813f14d05bd8
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2022-06-18 23:27:29 +0900

    Part 21 (bat wander state)

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -17,9 +17,11 @@ onready var player_detection_zone := $PlayerDetectionZone
 onready var anim_sprite := $AnimatedSprite
 onready var hurtbox = $Hurtbox
 onready var soft_collision = $SoftCollision
+onready var wander_controller = $WanderController
 
 
 func _ready() -> void:
+	state = pick_random_state([IDLE, WANDER])
 	anim_sprite.play("Animate")
 
 
@@ -30,19 +32,43 @@ func _physics_process(delta: float) -> void:
 	match state:
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION / 10)
+			if wander_controller.timer.is_stopped():
+				update_wander()
 
 		WANDER:
-			pass
+			if wander_controller.timer.is_stopped():
+				update_wander()
+			
+			accelerate_towards_point(wander_controller.target_position)
+			
+			# XXX: ?
+			if global_position.distance_to(wander_controller.target_position) <= MAX_SPEED:
+				update_wander()
 
 		CHASE:
 			if player != null:
-				var direction = global_position.direction_to(player.global_position)
-				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION)
-			anim_sprite.flip_h = velocity.x < 0
+				accelerate_towards_point(player.global_position)
+			else:
+				state = IDLE
 
 	if soft_collision.check_overlap():
 		velocity += soft_collision.get_push_vector() * delta * 400
 	velocity = move_and_slide(velocity)
+
+
+func update_wander() -> void:
+	state = pick_random_state([IDLE, WANDER])
+	wander_controller.start_wander_timer(rand_range(1, 3))
+
+
+func accelerate_towards_point(point: Vector2) -> void:
+	var direction = global_position.direction_to(point)
+	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION)
+	anim_sprite.flip_h = velocity.x < 0
+
+
+func pick_random_state(state_list: Array):
+	return state_list[randi() % state_list.size()]
 
 
 func _on_Hurtbox_area_entered(area: Hitbox) -> void:
